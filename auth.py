b62f77ad760311e8945f502c7d4a60312223fe79
Switi <45126465+pswiti@users.noreply.github.com>
2025-07-26 16:13:58 -0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
 from fastapi import Header, HTTPException, Security
 from fastapi.security.api_key import APIKeyHeader
+import os
 
 API_KEY = os.environ.get("API_KEY")  # Replace with a secure env-based approach
 API_KEY_NAME = "x-api-key"
